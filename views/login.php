63d7203dcea2c948414494378f8edf0a5f465e2b
rockingshan <shantanu.is.here@gmail.com>
2025-07-01 13:38:42 +0530

    Final version all working

--- views/login.php
+++ views/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - LCN Management</title>
-    <link rel="stylesheet" href="<?php echo BASE_PATH; ?>/css/login.css">
+    <link rel="stylesheet" href="<?php echo BASE_PATH; ?>/public/css/login.css">
 </head>
 <body>
     <?php if (isset($error)): ?>
